d3d136ef6bf18d013ab9d73f9f39716b5d19d118
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-05-05 06:15:35 +0000

    0.0.5

--- build.bash
+++ build.bash
@@ -16,14 +16,14 @@ OUTPUT_NAME cryptominisat5_simple
 LINK_FLAGS "\
     -s WASM=1 -s EXPORT_ES6=1 -s ALLOW_MEMORY_GROWTH=1 \
     -s EXPORTED_RUNTIME_METHODS='[\"ccall\", \"cwrap\"]' \
-    -s EXPORTED_FUNCTIONS=_cstart_solve,_ccontinue_solve,_cget_num_conflicts,_cmsat_new,_cmsat_free,_cmsat_nvars,_cmsat_add_clause,_cmsat_add_xor_clause,_cmsat_new_vars,_cmsat_solve,_cmsat_solve_with_assumptions,_cmsat_get_model,_cmsat_get_conflict,_cmsat_print_stats,_cmsat_set_num_threads,_cmsat_set_verbosity,_cmsat_set_default_polarity,_cmsat_set_no_simplify,_cmsat_set_no_simplify_at_startup,_cmsat_set_no_equivalent_lit_replacement,_cmsat_set_no_bva,_cmsat_set_no_bve,_cmsat_set_up_for_scalmc,_cmsat_simplify,_cmsat_set_max_time"
+    -s EXPORTED_FUNCTIONS=_malloc,_cstart_solve,_ccontinue_solve,_cget_num_conflicts,_cmsat_new,_cmsat_free,_cmsat_nvars,_cmsat_add_clause,_cmsat_add_xor_clause,_cmsat_new_vars,_cmsat_solve,_cmsat_solve_with_assumptions,_cmsat_get_model,_cmsat_get_conflict,_cmsat_print_stats,_cmsat_set_num_threads,_cmsat_set_verbosity,_cmsat_set_default_polarity,_cmsat_set_no_simplify,_cmsat_set_no_simplify_at_startup,_cmsat_set_no_equivalent_lit_replacement,_cmsat_set_no_bva,_cmsat_set_no_bve,_cmsat_set_up_for_scalmc,_cmsat_simplify,_cmsat_set_max_time"
 )
 
 SET_SOURCE_FILES_PROPERTIES(main_emscripten.cpp
     PROPERTIES COMPILE_FLAGS "\
     -s WASM=1 -s EXPORT_ES6=1 -s ALLOW_MEMORY_GROWTH=1 \
     -s EXPORTED_RUNTIME_METHODS='[\"ccall\", \"cwrap\"]' \
-    -s EXPORTED_FUNCTIONS=_cstart_solve,_ccontinue_solve,_cget_num_conflicts"
+    -s EXPORTED_FUNCTIONS=_malloc,_cstart_solve,_ccontinue_solve,_cget_num_conflicts"
 )
 EOF
 
